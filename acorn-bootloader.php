ca4350a360bb85f2a691bd194d375e31fbf5fa20
Mantas Tautvaisa <mantas.t@ipxo.com>
2025-04-22 11:29:09 +0300

    fix

--- acorn-bootloader.php
+++ acorn-bootloader.php
@@ -3,11 +3,12 @@
  * Plugin Name:  Acorn Bootloader
  * Plugin URI:   https://gitlab.com/aon21/acorn-bootloader
  * Description:  Acorn must be booted in order to use it. This plugin handles Acorn booting.
- * Version:      1.0.0
+ * Version:      1.0.1
  * Author:       Mantas Tautvaisa
  * License:      GPLv2 or later
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.html
  */
+ob_start();
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
@@ -36,28 +37,35 @@ function startSession(): void
     session()->put('_persistent', 'true');
     session()->save();
 
-    setcookie(
-        $session->getName(),
-        $session->getId(),
-        time() + (120 * 60),
-        "/",
-        "",
-        isset($_SERVER['HTTPS']),
-        true
-    );
+    if (!headers_sent()) {
+        setcookie(
+            $session->getName(),
+            $session->getId(),
+            time() + (120 * 60),
+            "/",
+            "",
+            isset($_SERVER['HTTPS']),
+            true
+        );
+    }
 }
 
-add_action('init', function() {
+
+add_action('init', function () {
 
     startSession();
 
     app('router')->pushMiddlewareToGroup('web', VerifyCsrfToken::class);
 });
 
-add_action('shutdown', function() {
+add_action('shutdown', function () {
     $session = app('session');
 
     if ($session->isStarted()) {
         $session->save();
+    }
+
+    if (ob_get_level() > 0) {
+        ob_end_flush();
     }
 });
